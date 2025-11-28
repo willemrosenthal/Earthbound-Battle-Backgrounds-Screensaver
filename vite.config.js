7926e1c9e2ddd0ac165edd1b055b4b3dbc947df7
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2025-11-28 14:51:43 -0500

    correct vite in prod

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import arraybuffer from 'vite-plugin-arraybuffer'
 
 export default defineConfig({
+  base: '/Earthbound-Battle-Backgrounds-JS/',
   plugins: [arraybuffer()]
 })

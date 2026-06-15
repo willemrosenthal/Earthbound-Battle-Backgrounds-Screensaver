9a2b034f0d9caf9943d130a1435c24e31cbe74f9
Willem Rosenthal <willemrosenthal@gmail.com>
2026-06-15 10:48:16 -0400

    xcode stuff

---
+++ EarthboundBattle/EarthboundBattle/EarthboundBattleView.h
@@ -0,0 +1,12 @@
+//
+//  EarthboundBattleView.h
+//  EarthboundBattle
+//
+//  Created by Willem Rosenthal on 6/15/26.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface EarthboundBattleView : ScreenSaverView
+
+@end
